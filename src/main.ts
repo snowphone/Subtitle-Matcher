f1f1b3ae6aabaf91247a1239bf59ad25268dd24e
Junoh Moon <mjo970625@gmail.com>
2020-02-18 00:40:48 +0900

    Clarify parser.parseArg's type using interface

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,17 @@ parser.addArgument(["-f", "--folder"], { help: "folder containing movies", defau
 parser.addArgument(['-v', "--video_ext"], { help: "video extension", choices: ["mkv", "mp4", "avi"], required: true });
 parser.addArgument(['-s', "--subtitle_ext"], { help: "subtitle extension", choices: ["smi", "srt"], defaultValue: "smi" });
 
-let argv = parser.parseArgs();
+interface ArgType {
+	folder: string
+	video_ext: string
+	subtitle_ext: string
+} 	//Always synchronize the interface with the parsing rule!
+
+let argv: ArgType = parser.parseArgs();
 main(argv);
 
 
-function main(argv: any) {
-	let matcher = new SubtitleMatcher(argv["video_ext"], argv["subtitle_ext"], argv["folder"]);
+function main(argv: ArgType) {
+	let matcher = new SubtitleMatcher(argv.video_ext, argv.subtitle_ext, argv.folder);
 	matcher.match();
 }
