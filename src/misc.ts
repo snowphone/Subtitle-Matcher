2e3ea8fb7c817d9972a5f6964d64323f557a4300
Junoh Moon <mjo970625@gmail.com>
2020-02-18 00:39:41 +0900

    Separate into export symbol and definition

--- src/misc.ts
+++ src/misc.ts
@@ -1,7 +1,10 @@
 
-export function zip<T, R>(lhs: Array<T>, rhs: Array<R>): Array<[T, R]> {
+export {zip}
+
+function zip<T, R>(lhs: Array<T>, rhs: Array<R>): Array<[T, R]> {
 	if (!lhs.length || !rhs.length) {
 		return [];
 	}
 	return [[lhs[0], rhs[0]], ...zip(lhs.slice(1), rhs.slice(1))];
 }
+
