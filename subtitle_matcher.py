d9a96383bba9eeff5561ea3c58bdf29190e6f297
Junoh Moon <jo.moon@dco.com>
2022-04-12 17:54:06 +0900

    Reformat code with black

--- subtitle_matcher.py
+++ subtitle_matcher.py
@@ -9,70 +9,85 @@ import tabulate
 
 
 class SubtitleMatcher:
-	def __init__(self, video_ext: str, folder: str, suffix: Optional[str] = None) -> None:
-		self.video_ext = video_ext
-		self.folder = folder
-		self.regexp = compile(f"{self.video_ext}$")
+    def __init__(
+        self, video_ext: str, folder: str, suffix: Optional[str] = None
+    ) -> None:
+        self.video_ext = video_ext
+        self.folder = folder
+        self.regexp = compile(f"{self.video_ext}$")
 
-		self.suffix = suffix
+        self.suffix = suffix
 
-	def match(self):
-		videos, subtitles = self._get_sorted_file_list()
+    def match(self):
+        videos, subtitles = self._get_sorted_file_list()
 
-		line_list = []
-		for v, s in zip(videos, subtitles):
-			subtitle_ext = s.split('.')[-1]
-			new_name = self._get_new_name(v, subtitle_ext)  # type: ignore
-			os.rename(s, new_name)
+        line_list = []
+        for v, s in zip(videos, subtitles):
+            subtitle_ext = s.split(".")[-1]
+            new_name = self._get_new_name(v, subtitle_ext)  # type: ignore
+            os.rename(s, new_name)
 
-			line_list.append( (os.path.basename(s) , os.path.basename(new_name)))
-		print(tabulate.tabulate(line_list, headers=["From", "To"], tablefmt="fancy_grid"))
+            line_list.append((os.path.basename(s), os.path.basename(new_name)))
+        print(
+            tabulate.tabulate(
+                line_list, headers=["From", "To"], tablefmt="fancy_grid"
+            )
+        )
 
-	def _get_sorted_file_list(self):
-		items = os.listdir(self.folder)
-		items = [ str(Path(self.folder).joinpath(it)) for it in items ]
+    def _get_sorted_file_list(self):
 
-		videos = [it for it in items if it.endswith(self.video_ext)]
-		subtitles = [it for it in items if it.endswith(".smi") or it.endswith(".srt")]
+        items = os.listdir(self.folder)
+        items = [str(Path(self.folder).joinpath(it)) for it in items]
 
-		videos.sort(key=NumericalSorter())
-		subtitles.sort(key=NumericalSorter())
+        videos = [it for it in items if it.endswith(self.video_ext)]
+        subtitles = [
+            it for it in items if it.endswith(".smi") or it.endswith(".srt")
+        ]
 
-		if(len(videos) != len(subtitles)):
-			raise RuntimeError("len(videos) != len(subtitles)")
+        videos.sort(key=NumericalSorter())
+        subtitles.sort(key=NumericalSorter())
 
-		return videos, subtitles
-	
-	def _get_new_name(self, src: str, subtitle_ext: Literal["smi", "srt"]) -> str:
-		if self.suffix:
-			return self.regexp.sub(f"{self.suffix}.{subtitle_ext}", src)
-		return self.regexp.sub(subtitle_ext, src)
+        if len(videos) != len(subtitles):
+            raise RuntimeError("len(videos) != len(subtitles)")
+
+        return videos, subtitles
+
+    def _get_new_name(
+        self, src: str, subtitle_ext: Literal["smi", "srt"]
+    ) -> str:
+        if self.suffix:
+            return self.regexp.sub(f"{self.suffix}.{subtitle_ext}", src)
+        return self.regexp.sub(subtitle_ext, src)
 
 
 class NumericalSorter:
-	def __call__(self, s: str):
-		return [self._try_int(i) for i in split(r"(\d+)", s)]
-	
-	@staticmethod
-	def _try_int(n: str):
-		if n.isdecimal():
-			return int(n)
-		return n
+    def __call__(self, s: str):
+        return [self._try_int(i) for i in split(r"(\d+)", s)]
 
-
+    @staticmethod
+    def _try_int(n: str):
+        if n.isdecimal():
+            return int(n)
+        return n
 
 
 def main(args):
-	s = SubtitleMatcher(args.video_ext, args.folder, args.suffix)
-	s.match()
+    s = SubtitleMatcher(args.video_ext, args.folder, args.suffix)
+    s.match()
 
 
 if __name__ == "__main__":
-	parser = ArgumentParser()
-	parser.add_argument("-f", "--folder", help="folder containing movies", default="./")
-	parser.add_argument("-v", "--video_ext", help="video extension", choices=["mkv", "mp4", "avi"], required=True)
-	parser.add_argument("--suffix", help="optional suffix", default=None)
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-f", "--folder", help="folder containing movies", default="./"
+    )
+    parser.add_argument(
+        "-v",
+        "--video_ext",
+        help="video extension",
+        choices=["mkv", "mp4", "avi"],
+        required=True,
+    )
+    parser.add_argument("--suffix", help="optional suffix", default=None)
 
-	main(parser.parse_args())
-
-
+    main(parser.parse_args())
--- test_subtitle_matcher.py
+++ test_subtitle_matcher.py
@@ -1,5 +1,6 @@
 from unittest.mock import MagicMock
 
+
 import pytest
 from pytest_mock import MockerFixture
 
@@ -12,81 +13,97 @@ VIDEO_EXT = "mp4"
 
 @pytest.fixture
 def sm():
-	return SubtitleMatcher(VIDEO_EXT, PATH_ROOT, LANG_SUFFIX)
+    return SubtitleMatcher(VIDEO_EXT, PATH_ROOT, LANG_SUFFIX)
 
 
 @pytest.fixture
 def mocked_listdir(mocker: MockerFixture):
-	video_src_list = [
-	    "hello_2people.mp4", "hello_11people.mp4", "hello_1people.mp4"
-	]
-	subtitle_src_list = ["chapter3.smi", "chapter2.smi", "chapter1.srt"]
+    video_src_list = [
+        "hello_2people.mp4",
+        "hello_11people.mp4",
+        "hello_1people.mp4",
+    ]
+    subtitle_src_list = ["chapter3.smi", "chapter2.smi", "chapter1.srt"]
 
-	mocked_listdir = mocker.patch("os.listdir")
-	mocked_listdir.return_value = video_src_list + subtitle_src_list
+    mocked_listdir = mocker.patch("os.listdir")
+    mocked_listdir.return_value = video_src_list + subtitle_src_list
 
-	return mocked_listdir
+    return mocked_listdir
 
 
 @pytest.fixture
 def mocked_rename(mocker: MockerFixture):
-	mocked_rename = mocker.patch("os.rename")
-	return mocked_rename
+    mocked_rename = mocker.patch("os.rename")
+    return mocked_rename
 
 
 def test_sort():
-	src = ["hello_2people", "hello_11people", "hello_1people"]
-	expected = ["hello_1people", "hello_2people", "hello_11people"]
+    src = ["hello_2people", "hello_11people", "hello_1people"]
+    expected = ["hello_1people", "hello_2people", "hello_11people"]
 
-	assert sorted(src) != expected
-	assert sorted(src, key=NumericalSorter()) == expected
-	return
+    assert sorted(src) != expected
+    assert sorted(src, key=NumericalSorter()) == expected
+    return
 
 
-@pytest.mark.parametrize("sm", [SubtitleMatcher(VIDEO_EXT, PATH_ROOT, None), SubtitleMatcher(VIDEO_EXT, PATH_ROOT, LANG_SUFFIX)])
+@pytest.mark.parametrize(
+    "sm",
+    [
+        SubtitleMatcher(VIDEO_EXT, PATH_ROOT, None),
+        SubtitleMatcher(VIDEO_EXT, PATH_ROOT, LANG_SUFFIX),
+    ],
+)
 def test_get_new_name(sm: SubtitleMatcher):
-	basename = "video1.helloworld"
-	subtitle_ext = "srt"
+    basename = "video1.helloworld"
+    subtitle_ext = "srt"
 
-	src = f"{basename}.{VIDEO_EXT}"
-	if sm.suffix:
-		expected = f"{basename}.{LANG_SUFFIX}.{subtitle_ext}"
-	else:
-		expected = f"{basename}.{subtitle_ext}"
+    src = f"{basename}.{VIDEO_EXT}"
+    if sm.suffix:
+        expected = f"{basename}.{LANG_SUFFIX}.{subtitle_ext}"
+    else:
+        expected = f"{basename}.{subtitle_ext}"
 
-	assert sm._get_new_name(src, subtitle_ext) == expected
-	return
+    assert sm._get_new_name(src, subtitle_ext) == expected
+    return
+
 
 def test_get_sorted_file_list(sm: SubtitleMatcher, mocked_listdir: MagicMock):
-	actual_vids, actual_subs = sm._get_sorted_file_list()
+    actual_vids, actual_subs = sm._get_sorted_file_list()
 
-	assert actual_vids == list(
-	    f"{PATH_ROOT}{it}" for it in
-	    ["hello_1people.mp4", "hello_2people.mp4", "hello_11people.mp4"])
-	assert actual_subs == list(
-	    f"{PATH_ROOT}{it}"
-	    for it in ["chapter1.srt", "chapter2.smi", "chapter3.smi"])
+    assert actual_vids == list(
+        f"{PATH_ROOT}{it}"
+        for it in [
+            "hello_1people.mp4",
+            "hello_2people.mp4",
+            "hello_11people.mp4",
+        ]
+    )
+    assert actual_subs == list(
+        f"{PATH_ROOT}{it}"
+        for it in ["chapter1.srt", "chapter2.smi", "chapter3.smi"]
+    )
 
-	mocked_listdir.assert_called_once_with(PATH_ROOT)
-	return
-
-
-def test_match(sm: SubtitleMatcher, mocked_rename: MagicMock,
-               mocked_listdir: MagicMock):
-	sm.match()
-
-	assert mocked_rename.call_count == len(mocked_listdir.return_value) // 2
-	mocked_listdir.assert_called_once_with(PATH_ROOT)
-	return
+    mocked_listdir.assert_called_once_with(PATH_ROOT)
+    return
 
 
-def test_match_with_insufficient_subtitles(sm: SubtitleMatcher,
-                                           mocked_rename: MagicMock,
-                                           mocked_listdir: MagicMock):
-	mocked_listdir.return_value = [
-	    it for it in mocked_listdir.return_value if not it.endswith("srt")
-	]
+def test_match(
+    sm: SubtitleMatcher, mocked_rename: MagicMock, mocked_listdir: MagicMock
+):
+    sm.match()
 
-	with pytest.raises(RuntimeError):
-		sm.match()
-	mocked_rename.assert_not_called()
+    assert mocked_rename.call_count == len(mocked_listdir.return_value) // 2
+    mocked_listdir.assert_called_once_with(PATH_ROOT)
+    return
+
+
+def test_match_with_insufficient_subtitles(
+    sm: SubtitleMatcher, mocked_rename: MagicMock, mocked_listdir: MagicMock
+):
+    mocked_listdir.return_value = [
+        it for it in mocked_listdir.return_value if not it.endswith("srt")
+    ]
+
+    with pytest.raises(RuntimeError):
+        sm.match()
+    mocked_rename.assert_not_called()
